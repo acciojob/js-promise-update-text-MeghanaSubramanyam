6475d02299764b2897db964cc663a9fbc019cdb8
MeghanaSubramanyam <152699209+MeghanaSubramanyam@users.noreply.github.com>
2025-05-11 22:51:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function sayHello() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000); // 1 second delay
+  });
+}
+
+sayHello().then((message) => {
+  document.getElementById("output").innerText = message;
+});
